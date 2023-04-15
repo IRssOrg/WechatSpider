a2db8606a609696b08ed44767a0956f4af1dff66
AvonNeuring <61532482+AvonNeuring@users.noreply.github.com>
2023-04-15 11:25:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,99 @@
-import json
 import requests
-import time
-import random
-import yaml
-from selenium import webdriver
+import redis
+import json
 import re
-
-AccountName=""
-input(AccountName)
-with open("wechat.yaml", "r") as file:
-    file_data = file.read()
-config = yaml.safe_load(file_data)
-
-headers = {
-    "Cookie": config['cookie'],
-    "User-Agent": config['user_agent']
-}
+import random
+import time
+class passage:
+    title=''
+    id=''
+    author=''
+    time=''
+    content=''
+    
 url = 'https://mp.weixin.qq.com'
-url_Search = "https://mp.weixin.qq.com/cgi-bin/searchbiz?"
-url_Article = "https://mp.weixin.qq.com/cgi-bin/appmsg"
+header = {
+    "HOST": "mp.weixin.qq.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"
+    }
+with open("app_msg_list.csv", "w",encoding='utf-8') as file:
+    file.write("文章标识符aid,标题title,链接url,时间time\n")
+i = 0
 
-def getToken():
-    resp = requests.get(url, headers=headers)
-    print(len(str(resp.url)))
-    print(resp.url)
-    token = re.findall(r'token=(\d+)', str(resp.url))[0]
+with open('cookie.txt', 'r', encoding='utf-8') as f:
+    cookie = f.read()
+cookies = json.loads(cookie)
+def gettoken():
+    response = requests.get(url=url, cookies=cookies)
+    token = re.findall(r'token=(\d+)', str(response.url))[0]
     return token
-def searchAccount(AccountName):
-    paramsSearch = {
-        'action': 'search_biz',
+def searchforfakeid(accountName):
+        query_id = {
+            'action': 'search_biz',
+            'token': gettoken(),
+            'lang': 'zh_CN',
+            'f': 'json',
+            'ajax': '1',
+            'random': random.random(),
+            'query': accountName,
+            'begin': '0',
+            'count': '5',
+        }
+        search_url = 'https://mp.weixin.qq.com/cgi-bin/searchbiz?'
+        search_response = requests.get(search_url, cookies=cookies, headers=header, params=query_id)
+        lists = search_response.json().get('list')[0]
+        fakeid = lists.get('fakeid')
+        return fakeid
+
+def articlespider(accoutName):
+    query_id_data = {
+        'token': gettoken(),
+        'lang': 'zh_CN',
+        'f': 'json',
+        'ajax': '1',
+        'random': random.random(),
+        'action': 'list_ex',
         'begin': '0',
         'count': '5',
-        'query': AccountName,
-        'token': getToken(),
-        'lang': 'zh_CN',
-
-        'f': 'json',
-        'ajax': '1'
+        'query': '',
+        'fakeid': searchforfakeid(accoutName),
+        'type': '9'
     }
-    fakeid='null'
-    search_resp = requests.get(url_Search, headers=headers, params=paramsSearch, verify=False)
-    if (search_resp.json() == 0):
-        print("未找到此公众号，请检查公众号名称是否正确")
-    else: print("成功找到公众号")
+    appmsg_url = 'https://mp.weixin.qq.com/cgi-bin/appmsg?'
+    appmsg_response = requests.get(appmsg_url, cookies=cookies, headers=header, params=query_id_data)
+    max_num = appmsg_response.json().get('app_msg_cnt')
+    num = int(int(max_num) / 5)
+    begin = 0
+    while num + 1 > 0 :
+        query_id_data = {
+            'token': gettoken(),
+            'lang': 'zh_CN',
+            'f': 'json',
+            'ajax': '1',
+            'random': random.random(),
+            'action': 'list_ex',
+            'begin': '{}'.format(str(begin)),
+            'count': '5',
+            'query': '',
+            'fakeid': searchforfakeid(accoutName),
+            'type': '9'
+        }
+        print('翻页###################',begin)
+        query_fakeid_response = requests.get(appmsg_url, cookies=cookies, headers=header, params=query_id_data)
+        fakeid_list = query_fakeid_response.json().get('app_msg_list')
+        for item in fakeid_list:
+            print(item.get('link'))
+            info = '"{}","{}","{}","{}"'.format(str(item["aid"]), item['title'], item['link'], str(item['create_time']))
+            with open("app_msg_list.csv", "a", encoding='utf-8') as f:
+                f.write(info + '\n')
 
-    lists = search_resp.json()
-    print(lists)
-    if 'fakeid' in lists:
-        fakeid = lists['fakeid']
+        num -= 1
+        begin = int(begin)
+        begin+=5
+        time.sleep(2)
 
-    else:
-        print('未找到公众号fakeid')
-    return fakeid
-
-def requestArticle(AccountName):
-    begin = "0"
-    params = {
-        "action": "list_ex",
-        "begin": begin,
-        "count": "5",
-        "fakeid": str(searchAccount(AccountName)),
-        "type": "9",
-        "token": config['token'],
-        "lang": "zh_CN",
-        "f": "json",
-        "ajax": "1"
-    }
-    i=0
-    while True:
-        begin = i * 5
-        params["begin"] = str(begin)
-        # 随机暂停几秒，避免过快的请求导致过快的被查到
-        time.sleep(random.randint(1, 10))
-        resp = requests.get(url_Article, headers=headers, params=params, verify=False)
-        # 微信流量控制, 退出
-        if resp.json()['base_resp']['ret'] == 200013:
-            print("frequencey control, stop at {}".format(str(begin)))
-            time.sleep(3600)
-            continue
-
-        # 如果返回的内容中为空则结束
-        if len(resp.json()['app_msg_list']) == 0:
-            print("all ariticle parsed")
-            break
-
-        msg = resp.json()
-        if "app_msg_list" in msg:
-            for item in msg["app_msg_list"]:
-                info = '"{}","{}","{}","{}"'.format(str(item["aid"]), item['title'], item['link'],
-                                                    str(item['create_time']))
-                with open("app_msg_list.csv", "a", encoding='utf-8') as f:
-                    f.write(info + '\n')
-            print(f"第{i}页爬取成功\n")
-            print("\n".join(info.split(",")))
-            print("\n\n---------------------------------------------------------------------------------\n")
-        i += 1
+articlespider('武汉大学')
 
 
-requestArticle("武汉大学")
 
 
