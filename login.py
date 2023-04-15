9965464d3f93a44dccf1fca0b056b31930e9cc09
AvonNeuring <61532482+AvonNeuring@users.noreply.github.com>
2023-04-15 11:24:57 +0800

    Update login.py

--- login.py
+++ login.py
@@ -5,17 +5,9 @@ from pprint import pprint
 
 post = {}
 
-driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
+driver = webdriver.Edge(executable_path="E:/edgedriver_win64/msedgedriver.exe")
 driver.get('https://mp.weixin.qq.com/')
 time.sleep(2)
-driver.find("./*//input[@id='account']").clear()
-driver.find_element_by_xpath("./*//input[@id='account']").send_keys('你的帐号')
-driver.find_element_by_xpath("./*//input[@id='pwd']").clear()
-driver.find_element_by_xpath("./*//input[@id='pwd']").send_keys('你的密码')
-# 在自动输完密码之后记得点一下记住我
-time.sleep(5)
-driver.find_element_by_xpath("./*//a[@id='loginBt']").click()
-# 拿手机扫二维码！
 time.sleep(15)
 driver.get('https://mp.weixin.qq.com/')
 cookie_items = driver.get_cookies()
